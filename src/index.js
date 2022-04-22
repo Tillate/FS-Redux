0d38f9b99f5f711fffa44bbb4d5ee7bf45381096
allan <allan.lelous@gmail.com>
2022-04-22 10:41:13 +0200

    Initialisation du projet "Les bases Redux Toolkit"

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./styles/index.css";
+import store from "./app/store";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={ store }/>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
