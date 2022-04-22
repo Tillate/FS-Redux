fc11261f3978a725c70a5f3f2d7ce072f8e64575
allan <allan.lelous@gmail.com>
2022-04-22 11:13:27 +0200

    ajout du useDispatch

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import picturesReducer from "../features/pictures.slice"
 
 export default configureStore ({
     reducer: {
-        pictures: null,
+        pictures: picturesReducer,
     },
 });
